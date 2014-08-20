6b3f14285580957c5b2f0fc19755ab11d62992b8
A. Stoewer <adrian.stoewer@rz.ifi.lmu.de>
2014-08-20 19:43:11 +0200

    example file

--- nix-full-example.py
+++ nix-full-example.py
@@ -20,7 +20,7 @@ delta = 0.1
 data = np.sin(np.arange(0, 10, delta)) * 10 - 60
 sigs = list()
 for i in range(4):
-    sig = rec01.create_data_array("Signal 0" + str(i+1), "analogsignal", DataType.Double, (len(data), ))
+    sig = rec01.create_data_array("Signal 0" + str(i + 1), "analogsignal", DataType.Double, (len(data), ))
     sig.data[:] = data
     sig.label = "Voltage"
     sig.unit  = "mV"
@@ -30,6 +30,9 @@ for i in range(4):
     dim.label = "Time"
     dim.unit  = "s"
     sigs.append(sig)
+
+# stimulus
+stim = rec01.create_data_array("Stimulus", "stimulus", DataType.Double, (len(data), ))
 
 # spiketrains
 spikes = list()
@@ -51,16 +54,27 @@ for i in range(4):
 # tag for the whole trial
 tag = rec01.create_tag("Trial 01", "trial", (0.0, ))
 tag.extent = (10, )
+feature = tag.create_feature(stim, LinkType.Tagged)
 for sig in sigs:
     tag.references.append(sig)
 for st in spikes:
     tag.references.append(st)
 
 # metadata for session
-tet_sec = f.create_section("Tetrode 01", "tetrode")
-tet_sec.create_property("Manufacturer", (Value("xzy"), ))
-tet_sec.create_property("ElectrodeCount", (Value(4), ))
+rec_sec = f.create_section("Session 01", "recordingsession")
+rec_sec.create_property("Experimenter", (Value("Thomas Wachtler"), ))
+rec01.metadata = rec_sec
 
+tet_sec = rec_sec.create_section("Tetrode 01", "tetrode")
+tet_sec.create_property("Manufacturer", (Value("Thomas Recordings"), ))
+tet_sec.create_property("ElectrodeCount", (Value(4), ))
 tet.metadata = tet_sec
+
+sub_sec = rec_sec.create_section("Subject 01", "subject")
+sub_sec.create_property("Species", (Value("Macaca mulatta"), ))
+sub_sec.create_property("Name", (Value("Maoli"), ))
+sub_sec.create_property("Age", (Value(6), ))
+
+
 
 f.close()
